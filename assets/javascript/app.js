333a8e4d72a2f61ec2113fcca2320ac9d5bebcbd
Tran <Tran.Judie@gmail.com>
2018-03-20 21:09:42 -0400

    added state and fix some layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,13 @@
 
 jQuery(document).ready(function($) {
   $.ajax({
-  url : "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
-  dataType : "jsonp",
-  success : function(parsed_json) {
-  var location = parsed_json['location']['city'];
-  var temp_f = parsed_json['current_observation']['temp_f'];
-  alert("Current temperature in " + location + " is: " + temp_f);
-  }
+    url:
+      "http://api.wunderground.com/api/2a101c3a4d1a6ce9/geolookup/conditions/q/IA/Cedar_Rapids.json",
+    dataType: "jsonp",
+    success: function(parsed_json) {
+      var location = parsed_json["location"]["city"];
+      var temp_f = parsed_json["current_observation"]["temp_f"];
+      alert("Current temperature in " + location + " is: " + temp_f);
+    }
   });
 });
