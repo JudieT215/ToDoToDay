9fe7e4241b9c732bdf9fbbb4879b4ea60ad60e63
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-03-22 20:09:58 -0400

    Added jQuery for user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,8 +22,10 @@ console.log (url);
 
 
     // Google Places code
-    var apiKey = "AIzaSyBvUORzXVi9vPlOAOl3N4kmruWeQ52VZk0";
-    var api_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&key=" + apiKey;
+    var apiKeyG = "AIzaSyBvUORzXVi9vPlOAOl3N4kmruWeQ52VZk0";
+    var placeLoc = "-33.8670522,151.1957362";
+    var placeType = ""; // Optional for now
+    var api_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=500&location=" + placeLoc + "&type=" + placeType + "&key=" + apiKeyG;
     var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/';
     var request_url = cors_anywhere_url + api_url;
     $.ajax({
@@ -36,9 +38,9 @@ console.log (url);
         //     console.log(error);
         // })
         .done(function(data) {
-            console.log(data);
+            console.log(data.results);
         });
-        
+
     // This part doesn't work because of CORS
     // var apiKey = "AIzaSyBvUORzXVi9vPlOAOl3N4kmruWeQ52VZk0";
     // $.ajax({
@@ -47,6 +49,11 @@ console.log (url);
     // }).then(function(response) {
     //     console.log(response);
     // });
+
+    // Capture user input
+    city = $("#City-Name").val().trim();
+    state = $("#State").val().trim();
+    console.log("CITY: " + city + ", STATE: " + state);
 
 });
 
