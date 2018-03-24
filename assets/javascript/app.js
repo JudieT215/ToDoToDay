5187a27705420eb8f9060031aed8b74fd530bd17
Tran <Tran.Judie@gmail.com>
2018-03-24 10:02:46 -0400

    added image folder

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,10 @@
 
 
 jQuery(document).ready(function ($) {
+//gobal var
+    var stateList = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"];
+
+
 
   //This is our API Key
   var APIKeyW = "2a101c3a4d1a6ce9"
