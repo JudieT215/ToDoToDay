6a46d8cd83cd7d25046c1554002740e80df0266a
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-03-24 10:43:56 -0400

    Adding Geocoding API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,36 +40,51 @@ jQuery(document).ready(function ($) {
 
 
 
+    // Google Maps Geocode API request
+    function geoCode() {
+        var apiKeyG = "AIzaSyBvUORzXVi9vPlOAOl3N4kmruWeQ52VZk0";
+        var api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA" + "&key=" + apiKeyG;
+        var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/';
+        var request_url = cors_anywhere_url + api_url;
+        $.ajax({
+            url: request_url,
+            context: document.body
+        })
+            .done(function(data) {
+                console.log(data.results);
+            });
+    }
 
+    // Google Places API request
+    function gPlace() {
+        var apiKeyG = "AIzaSyBvUORzXVi9vPlOAOl3N4kmruWeQ52VZk0";
+        var placeLoc = "-33.8670522,151.1957362";
+        var placeType = ""; // Optional for now
+        var api_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=500&location=" + placeLoc + "&type=" + placeType + "&key=" + apiKeyG;
+        var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/';
+        var request_url = cors_anywhere_url + api_url;
+        $.ajax({
+            url: request_url,
+            context: document.body
+        })
+            // This doesn't seem to work
+            // .error(function(error) {
+            //     console.log('error!');
+            //     console.log(error);
+            // })
+            .done(function(data) {
+                console.log(data.results);
+            });
 
-    // Google Places code
-    var apiKeyG = "AIzaSyBvUORzXVi9vPlOAOl3N4kmruWeQ52VZk0";
-    var placeLoc = "-33.8670522,151.1957362";
-    var placeType = ""; // Optional for now
-    var api_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=500&location=" + placeLoc + "&type=" + placeType + "&key=" + apiKeyG;
-    var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/';
-    var request_url = cors_anywhere_url + api_url;
-    $.ajax({
-        url: request_url,
-        context: document.body
-    })
-        // This doesn't seem to work
-        // .error(function(error) {
-        //     console.log('error!');
-        //     console.log(error);
-        // })
-        .done(function(data) {
-            console.log(data.results);
-        });
-
-    // This part doesn't work because of CORS
-    // var apiKey = "AIzaSyBvUORzXVi9vPlOAOl3N4kmruWeQ52VZk0";
-    // $.ajax({
-    //     url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&key=" + apiKey,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(response);
-    // });
+        // This part doesn't work because of CORS
+        // var apiKey = "AIzaSyBvUORzXVi9vPlOAOl3N4kmruWeQ52VZk0";
+        // $.ajax({
+        //     url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&key=" + apiKey,
+        //     method: "GET"
+        // }).then(function(response) {
+        //     console.log(response);
+        // });
+    }
 
     // Capture user input
     function getInput() {
@@ -77,11 +92,13 @@ jQuery(document).ready(function ($) {
         state = $("#State").val().trim();
         console.log("CITY: " + city + ", STATE: " + state); 
     }
-    
-    // Submit button to add new City/State
+
+    // Submit button to add new City/State and run queries
     $("#submit-button").on("click", function(event) {
         event.preventDefault();
         getInput();
+        geoCode();
+        gPlace();
     });
 
     // On load, set focus on City input box
